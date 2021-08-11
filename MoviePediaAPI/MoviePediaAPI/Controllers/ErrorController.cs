fe459cbc46fe8a7729cc8649505aa8bc9c645fb2
Subhajit Saha <Subhajit.Saha1@gds.ey.com>
2021-08-11 11:15:47 +0530

    Add error handling capabilities

---
+++ MoviePediaAPI/MoviePediaAPI/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace MoviePediaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        private readonly ILogger<ErrorController> _logger;
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var reqPathErrorContext = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var details = reqPathErrorContext.Error.StackTrace ?? "Malpractice behaviour detected";
+            var instance = reqPathErrorContext.Path ?? "Malpractice behaviour detected";
+            var title = reqPathErrorContext.Error.Message ?? "Malpractice behaviour detected";
+            var type = reqPathErrorContext.Error.GetType().Name ?? "Malpractice behaviour detected";
+            var traceid = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            int statusCode = 500;
+
+            _logger.LogError(reqPathErrorContext.Error, title, details, traceid);
+
+            return Problem("Please contact administrator", instance, statusCode, "Something went wrong", type);
+        }
+    }
+}
--- MoviePediaAPI/MoviePediaAPI/Startup.cs
+++ MoviePediaAPI/MoviePediaAPI/Startup.cs
@@ -1,17 +1,9 @@
 using Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistance;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MoviePediaAPI
 {
@@ -35,10 +27,7 @@ namespace MoviePediaAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler("/api/error/");
 
             app.UseHttpsRedirection();
 
