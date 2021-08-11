faae5e835d034f9ad10b9a9520d7ea37c417c980
Subhajit Saha <Subhajit.Saha1@gds.ey.com>
2021-08-11 11:32:21 +0530

    Created component structure

--- MoviepediaUI/src/app/app-routing.module.ts
+++ MoviepediaUI/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { MoviesComponent } from './movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch:'full'
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+    path: 'movies/:id',
+    component: MovieDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MoviepediaUI/src/app/app.module.ts
+++ MoviepediaUI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieDetailsComponent    
   ],
   imports: [
     BrowserModule,
